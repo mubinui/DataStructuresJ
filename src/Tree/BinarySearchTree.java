f9456bb64f7273fce03d28faec2d7774299b229f
uicmu <mubin41@student.sust.edu>
2022-10-03 05:40:29 +0600

    Tree properties optimized and non optimized diameter

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -67,8 +67,57 @@ public class BinarySearchTree {
         // the recursive method will iterate through the whole tree
         // so even the leaves will be considered as root
     }
-//    public int countHeight(Node root){
-//        if (root==null) return
-//    }
+    // height is the deepest path between the root and the leaves
+    public int countHeight(Node root){
+        if (root==null) return 0;
+        int leftHeight = countHeight(root.left); // height of the left subtree.
+        int rightHeight = countHeight(root.right); // height of the right subtree
+        return Math.max(leftHeight,rightHeight)+1; // the +1 is for the root node
+
+    }
+    // Diameter of a tree .
+     // Number of Nodes in the longest path between any two nodes
+    // case 01 : Diameter through root
+    // Case 02 : Diameter not through root
+    public int diameterA01 (Node root ){
+        // 1. diameter in left subtree
+        // 2. diameter in right subtree
+        // 3. diameter in left_height+right_height+1
+        // max of these three will be the diameter of our actual tree
+        if (root == null ) return 0;
+        int leftSubDiam = diameterA01(root.left);
+        int rightSubDiam = diameterA01(root.right);
+        int height = countHeight(root.left)+countHeight(root.right);
+        return Math.max(Math.max(leftSubDiam,rightSubDiam),height+1);
+
+    }
+    public class TreeInfo{
+        int height;
+        int diam;
+
+        TreeInfo(int height, int diam){
+            this.height = height;
+            this.diam = diam;
+
+        }
+
+    }
+    // calculating diameter and height together
+    public TreeInfo optimized_diameter(Node root){
+        if (root == null) return new TreeInfo(0,0);
+        // Tree info
+        //Calculating height
+        TreeInfo left = optimized_diameter(root.left);
+        TreeInfo right = optimized_diameter(root.right);
+        int treeHeight = Math.max(left.height, right.height)+1;
+        // determining the diameter of a tree
+        int diam1 = left.diam;
+        int diam2 = right.diam;
+        int diam3 = left.height + right.height + 1;
+        int outDiam = Math.max(Math.max(diam1,diam2),diam3);
+        TreeInfo output = new TreeInfo(treeHeight,outDiam);
+        return output;
+
+    }
 
 }
--- src/Tree/TreeTester.java
+++ src/Tree/TreeTester.java
@@ -3,7 +3,8 @@ package Tree;
 public class TreeTester {
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
-        int [] array = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int [] array = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,7,-1,-1};
+        int [] tree = {1, 2, 4, -1, -1, 5, 8, -1, -1, 9, -1, -1, 3, 6, -1, 10, -1, -1, 7, -1, -1};
         Node root = bst.treeBuilder(array);
         bst.levelOrder(root);
         System.out.println("___________________>");
@@ -17,5 +18,14 @@ public class TreeTester {
         System.out.println();
         System.out.println("------------------------------>");
         bst.postorder(root);
+        System.out.println();
+        System.out.println("The height of the tree "+ bst.countHeight(root));
+        System.out.println("------------------------------------------------>");
+        System.out.println("____________Tree test 02___________________");
+        BinarySearchTree binary_tree = new BinarySearchTree();
+        Node b_root = binary_tree.treeBuilder(tree);
+        System.out.println("Diameter of the tree "+binary_tree.diameterA01(b_root));
+        System.out.println("Diameter of the tree using "+binary_tree.optimized_diameter(b_root).diam);
+
     }
 }
